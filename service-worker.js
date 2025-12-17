bd679cc298e27becc967bf1c9a37b483833627d2
raju-grocery <rg681528@gmail.com>
2025-12-17 11:20:18 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,13 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("raju-grocery").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./raju.jpg",
-        "./atta.jpg",
-        "./rice.jpg",
-        "./oil.jpg"
-      ]);
-    })
-  );
+self.addEventListener("install",e=>{
+ e.waitUntil(
+  caches.open("raju-grocery").then(c=>{
+   return c.addAll(["./","./index.html","./manifest.json"]);
+  })
+ );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+ );
 });
